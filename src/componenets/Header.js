7b2fddd96d3f3ccd775059cf451488c6b8ae36b6
charliebarger <charliebarger04@gmail.com>
2021-07-06 08:50:20 -0600

    added images

---
+++ src/componenets/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App";
+
+export default function Header() {
+  return (
+    <div className={"Header"}>
+      {/* <div className = {'image-wrapper'}>
+            <img alt = {'South Park Sign'}src = {process.env.PUBLIC_URL + './images/sign.png'}/>
+            </div> */}
+      <p>South Park Memory Game</p>
+    </div>
+  );
+}
