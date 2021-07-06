ddd1a7ee639eb7d3180835bf0e2975eab77a8e25
charliebarger <charliebarger04@gmail.com>
2021-07-06 08:50:01 -0600

    created initial styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
-
+import "./App.css";
+import React from "react";
+import Header from "./componenets/Header";
 function App() {
   return (
     <div className="App">
-      Hi
+      <Header />
+      <div className="scoreboard">
+        <span>Current Score: 0</span>
+        <div className="flex-gap"></div>
+        <span>Best Score: 0</span>
+      </div>
+      <div className="gamebaord-wrapper">
+        <div className="gameboard"></div>
+      </div>
     </div>
   );
 }
