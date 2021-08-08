f06da13f5ea03316fa0c1ae96aeec6fa4a3e3670
charliebarger <charliebarger04@gmail.com>
2021-08-08 11:46:41 -0600

    removed unused img

--- src/componenets/Header.js
+++ src/componenets/Header.js
@@ -4,9 +4,6 @@ import "../App";
 export default function Header() {
   return (
     <div className={"Header"}>
-      {/* <div className = {'image-wrapper'}>
-            <img alt = {'South Park Sign'}src = {process.env.PUBLIC_URL + './images/sign.png'}/>
-            </div> */}
       <p>South Park Memory Game</p>
     </div>
   );
