b6425537854a454d313d3b8e66cf4f30d2cc620e
charliebarger <charliebarger04@gmail.com>
2021-08-09 12:21:16 -0600

    clean code

--- src/componenets/Scoreboard.js
+++ src/componenets/Scoreboard.js
@@ -2,15 +2,14 @@ import React, { useState } from "react";
 import "../App";
 
 export default function Scoreboard(props) {
-  const characters = props.characters;
-  let currentScore = 0;
   const [highScore, setHighScore] = useState(props.currentScore);
+  const currentScore = props.currentScore;
   if (props.currentScore > highScore) {
-    setHighScore(props.currentScore);
+    setHighScore(currentScore);
   }
   return (
     <div className="scoreboard">
-      <span>Current Score: {props.currentScore}</span>
+      <span>Current Score: {currentScore}</span>
       <div className="flex-gap"></div>
       <span>High Score: {highScore}</span>
     </div>
